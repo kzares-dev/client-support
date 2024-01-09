e86a9c9ce48b7edf894501788ed7cebf405cec41
kzares-dev <138066850+kzares-dev@users.noreply.github.com>
2024-01-09 18:49:53 -0500

    migrating to nextjs v14

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SignIn() {
   return (
-    <div>
+    <div className="w-full max-w-[450px] bg-white min-h-[600px] shadow-lg border rounded-xl px-4 py-5 ">
         
     </div>
   )
